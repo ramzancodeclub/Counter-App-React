8960a6b5fa895daa245071e3996ac9fcf56e6b1a
Ramzan <mramzanstv@gmail.com>
2024-04-16 11:39:37 +0500

    clean repository

--- src/tests/example.test.jsx
+++
@@ -1,8 +0,0 @@
-import { describe, it, expect } from "vitest";
-
-describe("Example Suite", () => {
-    it("should pass", () => {
-        const sum = 1 + 1
-        expect(sum).toBe(2)
-    })
-})
