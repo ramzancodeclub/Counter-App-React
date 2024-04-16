4acb6be11a52596f7a8ee5327fb7616e2402441d
Ramzan <mramzanstv@gmail.com>
2024-04-16 07:51:41 +0500

    upload files with src etc..

---
+++ src/tests/example.test.jsx
@@ -0,0 +1,8 @@
+import { describe, it, expect } from "vitest";
+
+describe("Example Suite", () => {
+    it("should pass", () => {
+        const sum = 1 + 1
+        expect(sum).toBe(2)
+    })
+})
